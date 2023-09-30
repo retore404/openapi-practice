2a3acbf6642680f02345146e176cfa4c65fee313
retore404 <44674058+retore404@users.noreply.github.com>
2023-09-30 12:32:26 +0900

    add: Spring initialize

---
+++ app/src/main/java/jp/retore/openapipractice/OpenapipracticeApplication.java
@@ -0,0 +1,13 @@
+package jp.retore.openapipractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapipracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapipracticeApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/jp/retore/openapipractice/OpenapipracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.retore.openapipractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapipracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
